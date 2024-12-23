5c98e8c01ee1741064f57a37b9d25a825bb9e3fa
Tim Usner <tim.usner@sap.com>
2024-12-23 16:42:41 +0100

    WIP: Add example chart

--- example/operator/doc.go
+++ example/operator/doc.go
@@ -5,6 +5,7 @@
 //go:generate ../../hack/generate-crds.sh -p 10-crd- --allow-dangerous-types operator.gardener.cloud
 //go:generate cp 10-crd-operator.gardener.cloud_gardens.yaml ../../charts/gardener/operator/templates/crd-gardens.yaml
 //go:generate cp 10-crd-operator.gardener.cloud_extensions.yaml ../../charts/gardener/operator/templates/crd-extensions.yaml
+//go:generate sh -c "helm template ../../hack/extensions/registration > 15-extension.yaml"
 
 // Package operator contains example manifests for working on operator.
 package operator
