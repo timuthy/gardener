e4c60f7716ca38ed97e12b35fe610b801148c46f
Tim Usner <tim.usner@sap.com>
2025-02-04 16:47:41 +0100

    WIP: Refine script and components

--- hack/generate-kustomize-base-extension.sh
+++ hack/generate-kustomize-base-extension.sh
@@ -6,9 +6,33 @@
 set -e
 set -o pipefail
 
+function usage {
+    cat <<EOM
+Usage:
+generate-kustomize-base-extension [options]
+
+This script generates a kustomization.yaml file for building an Extension (operator.gardener.cloud) manifest.
+
+    -h, --help                              Display this help and exit.
+    --name                                  Name is the name of the extension.
+    --provider-type                         Type of the provider.
+    --component-name                        Name of the Kustomize component, one of {provider-extension, network, containerruntime, extension}
+    --destination                           The path the kustomization.yaml is written to.
+    --extension-oci-repository              URL to OCI image containing the extension chart.
+    --admission-runtime-oci-repository      OPTIONAL: URL to OCI image containing the admission runtime chart.
+    --admission-application-oci-repository  OPTIONAL: URL to OCI image containing the admission application chart.
+    --version                               OPTIONAL: Version of Gardener containing example files.
+    --resources-path                        OPTIONAL: Path to Gardener repository. Upstream https://github.com/gardener/gardener will be used if not defined.
+EOM
+    exit 0
+}
+
 # Parse the arguments
 for i in "$@"; do
     case $i in
+    -h|--help)
+        usage
+        ;;
     --name=*)
         NAME="${i#*=}"
         shift
@@ -29,13 +53,26 @@ for i in "$@"; do
         COMPONENT_NAME="${i#*=}"
         shift
         ;;
+    --resources-path=*)
+        RESOURCES_PATH="${i#*=}"
+        shift
+        ;;
+    --admission-runtime-oci-repository=*)
+        ADMISSION_RUNTIME_OCI_REPO="${i#*=}"
+        shift
+        ;;
+    --admission-application-oci-repository=*)
+        ADMISSION_APP_OCI_REPO="${i#*=}"
+        shift
+        ;;
+    --version=*)
+        VERSION="${i#*=}"
+        shift
+        ;;
     esac
 done
 
-if [ -z "${NAME:-}" ] || [ -z "${PROVIDER_TYPE:-}" ] || [ -z "${OCI_REPO:-}" ] || [ -z "${DESTINATION:-}" ] || [ -z "${COMPONENT_NAME:-}" ]; then
-  echo "One or more parameters are missing."
-  exit 1
-fi
+( [[ -z "${NAME:-}" ]] || [[ -z "${PROVIDER_TYPE:-}" ]] || [[ -z "${OCI_REPO:-}" ]] || [[ -z "${DESTINATION:-}" ]] || [[ -z "${COMPONENT_NAME:-}" ]] ) && usage
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 COMPONENT_PATH="${SCRIPT_DIR}/../example/extensions/components/${COMPONENT_NAME}"
@@ -45,46 +82,59 @@ if [ ! -d "${COMPONENT_PATH}" ]; then
   exit 1
 fi
 
-# Find number of resources requiring patches.
-NUMBER_OF_RESOURCES=$(kustomize build "${COMPONENT_PATH}" | yq -r '.spec.resources | length')
+if [[ -n $VERSION ]]; then
+  VERSION_REF="?ref=${VERSION}"
+fi
 
-# Add patch for extension name
-PATCHES="  - op: replace
-    path: /metadata/name
-    value: $NAME
-"
-
-# Create type patches.
-for ((i = 0; i < $NUMBER_OF_RESOURCES; i++)); do
-    PATCHES+="  - op: replace
-    path: /spec/resources/$i/type
-    value: $PROVIDER_TYPE
-"
-done
-
-# Add the patch for oci repository.
-PATCHES+="  - op: replace
-    path: /spec/deployment/extension/helm/ociRepository
-    value: $OCI_REPO
-"
-
-# Construct the Kustomization file
-cat <<EOF >$DESTINATION/kustomization.yaml
-apiVersion: kustomize.config.k8s.io/v1beta1
+# Kustomization file
+KUSTOMIZATION_YAML="apiVersion: kustomize.config.k8s.io/v1beta1
 kind: Kustomization
 
 resources:
-- ../base
+- ${RESOURCES_PATH:="https://github.com/gardener/gardener/"}/example/extensions/base/${VERSION_REF:-""}
 
 components:
-- ../components/provider-extension
+- ${RESOURCES_PATH}/example/extensions/components/${COMPONENT_NAME}/${VERSION_REF:-""}
 
 patches:
 - target:
     version: v1alpha1
     group: operator.gardener.cloud
     kind: Extension
-    name: $NAME
+    name: example-extension
   patch: |-
-$PATCHES
-EOF
+    - op: replace
+      path: /metadata/name
+      value: $NAME
+    - op: replace
+      path: /spec/deployment/extension/helm/ociRepository
+      value: $OCI_REPO
+"
+
+# Add admission component.
+if [[ -n $ADMISSION_RUNTIME_OCI_REPO ]] && [[ -n $ADMISSION_APP_OCI_REPO ]]; then
+  KUSTOMIZATION_YAML=$(printf "$KUSTOMIZATION_YAML" | yq ".components += \"${RESOURCES_PATH}/example/extensions/components/admission/${VERSION_REF:-""}\"")
+
+  KUSTOMIZATION_YAML=$(printf "$KUSTOMIZATION_YAML" | yq ".patches[0].patch +=\"
+- op: replace
+  path: /spec/deployment/admission/runtimeCluster/helm/ociRepository
+  value: ${ADMISSION_RUNTIME_OCI_REPO}
+- op: replace
+  path: /spec/deployment/admission/virtualCluster/helm/ociRepository
+  value: ${ADMISSION_APP_OCI_REPO}
+\"")
+fi
+
+# Create extension type patches.
+NUMBER_OF_RESOURCES=$(yq -r '.spec.resources | length' "${COMPONENT_PATH}/extension.yaml")
+
+for ((i = 0; i < $NUMBER_OF_RESOURCES; i++)); do
+    KUSTOMIZATION_YAML=$(printf "$KUSTOMIZATION_YAML" | yq ".patches[0].patch +=\"
+- op: replace
+  path: /spec/resources/${i}/type
+  value: ${PROVIDER_TYPE}
+\"")
+done
+
+# Write result to destination.
+printf "$KUSTOMIZATION_YAML" > $DESTINATION/kustomization.yaml
