c5af6b9cf04c4c3b4f4609b7068faea8c7f5e0ad
Tim Usner <tim.usner@sap.com>
2025-05-14 16:44:50 +0200

    WIP: Refine defaulting

--- pkg/operator/webhook/defaulting/extension/handler_test.go
+++ pkg/operator/webhook/defaulting/extension/handler_test.go
@@ -10,7 +10,10 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gstruct"
+	admissionv1 "k8s.io/api/admission/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	operatorv1alpha1 "github.com/gardener/gardener/pkg/apis/operator/v1alpha1"
@@ -47,42 +50,42 @@ var _ = Describe("Handler", func() {
 				extension.Spec.Resources = nil
 				extension.Spec.Deployment.ExtensionDeployment.InjectGardenKubeconfig = nil
 
-				Expect(handler.Default(ctx, extension)).To(Succeed())
+				Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 				Expect(extension.Spec.Deployment.ExtensionDeployment.InjectGardenKubeconfig).To(BeNil())
 			})
 
 			It("should do nothing if the deployment section is not set", func() {
 				extension.Spec.Deployment = nil
 
-				Expect(handler.Default(ctx, extension)).To(Succeed())
+				Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 				Expect(extension.Spec.Deployment).To(BeNil())
 			})
 
 			It("should do nothing if the extension deployment section is not set", func() {
 				extension.Spec.Deployment.ExtensionDeployment = nil
 
-				Expect(handler.Default(ctx, extension)).To(Succeed())
+				Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 				Expect(extension.Spec.Deployment.ExtensionDeployment).To(BeNil())
 			})
 
 			It("should do nothing if injectGardenKubeconfig is already set", func() {
 				extension.Spec.Deployment.ExtensionDeployment.InjectGardenKubeconfig = ptr.To(false)
 
-				Expect(handler.Default(ctx, extension)).To(Succeed())
+				Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 				Expect(extension.Spec.Deployment.ExtensionDeployment.InjectGardenKubeconfig).To(PointTo(BeFalse()))
 			})
 
 			It("should do default the injectGardenKubeconfig to true", func() {
 				extension.Spec.Deployment.ExtensionDeployment.InjectGardenKubeconfig = nil
 
-				Expect(handler.Default(ctx, extension)).To(Succeed())
+				Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 				Expect(extension.Spec.Deployment.ExtensionDeployment.InjectGardenKubeconfig).To(PointTo(BeTrue()))
 			})
 		})
 
 		Context("primary defaulting", func() {
 			It("should default the primary field to true", func() {
-				Expect(handler.Default(ctx, extension)).To(Succeed())
+				Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 				Expect(extension.Spec.Resources).To(ConsistOf(
 					gardencorev1beta1.ControllerResource{
 						Kind:    "Worker",
@@ -95,7 +98,7 @@ var _ = Describe("Handler", func() {
 			It("should not overwrite the primary field", func() {
 				extension.Spec.Resources[0].Primary = ptr.To(false)
 
-				Expect(handler.Default(ctx, extension)).To(Succeed())
+				Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 				Expect(extension.Spec.Resources).To(ConsistOf(
 					gardencorev1beta1.ControllerResource{
 						Kind:    "Worker",
@@ -107,13 +110,13 @@ var _ = Describe("Handler", func() {
 		})
 
 		Context("autoEnable defaulting", func() {
-			Context("kind == Extension", func() {
+			When("kind == Extension", func() {
 				BeforeEach(func() {
 					extension.Spec.Resources[0].Kind = "Extension"
 				})
 
 				It("should default the autoEnable field to none", func() {
-					Expect(handler.Default(ctx, extension)).To(Succeed())
+					Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 					Expect(extension.Spec.Resources).To(ConsistOf(
 						gardencorev1beta1.ControllerResource{
 							Kind:       "Extension",
@@ -127,7 +130,7 @@ var _ = Describe("Handler", func() {
 				It("should default the autoEnable field to shoot", func() {
 					extension.Spec.Resources[0].GloballyEnabled = ptr.To(true)
 
-					Expect(handler.Default(ctx, extension)).To(Succeed())
+					Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 					Expect(extension.Spec.Resources).To(ConsistOf(
 						gardencorev1beta1.ControllerResource{
 							Kind:            "Extension",
@@ -143,7 +146,7 @@ var _ = Describe("Handler", func() {
 					extension.Spec.Resources[0].GloballyEnabled = ptr.To(true)
 					extension.Spec.Resources[0].AutoEnable = []gardencorev1beta1.ClusterType{"seed"}
 
-					Expect(handler.Default(ctx, extension)).To(Succeed())
+					Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 					Expect(extension.Spec.Resources).To(ConsistOf(
 						gardencorev1beta1.ControllerResource{
 							Kind:            "Extension",
@@ -156,9 +159,9 @@ var _ = Describe("Handler", func() {
 				})
 			})
 
-			Context("kind != Extension", func() {
+			When("kind != Extension", func() {
 				It("should not overwrite the autoEnable field", func() {
-					Expect(handler.Default(ctx, extension)).To(Succeed())
+					Expect(handler.Handle(ctx, admission.Request{AdmissionRequest: admissionv1.AdmissionRequest{Object: runtime.RawExtension{Object: extension}}})).To(Succeed())
 					Expect(extension.Spec.Resources).To(ConsistOf(
 						gardencorev1beta1.ControllerResource{
 							Kind:       "Worker",
